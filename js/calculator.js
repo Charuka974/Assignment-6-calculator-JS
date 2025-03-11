93152ba798f9ba67240a1dded08fc5da3dfa122c
Charuka974 <charuka974@gmail.com>
2025-03-11 12:19:27 +0530

    improving the backend and combining into one function

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,29 @@
-function calculate () {
-    let expression = document.getElementById('display').value;
-    try {
-        let result = eval(expression);
-        console.log(result);
-        document.getElementById('display').value = result;
-    } catch (error) {
-        document.getElementById('display').value = "Syntax Error";
-    }
-}
-
 function clickButton (symbol) {
-    let result = symbol;
-    document.getElementById('display').value += result;     
-}
 
-function clearDisplay () {
-    document.getElementById('display').value = "";
+    if (symbol === 'c') {
+
+        document.getElementById('display').value = "";
+
+    } else if (symbol === '=') {
+
+        let expression = document.getElementById('display').value;
+        if (expression === "" || expression === "+" || expression === "-" || expression === "/" || expression === "*" || expression === ".") {
+            document.getElementById('display').value = "";
+        } else {
+            try {
+                let result = eval(expression);
+                console.log(result);
+                document.getElementById('display').value = result;
+            } catch (error) {
+                document.getElementById('display').value = "Syntax Error";
+            }
+        }   
+
+    } else {
+
+        let result = symbol;
+        document.getElementById('display').value += result;  
+
+    }   
+   
 }
